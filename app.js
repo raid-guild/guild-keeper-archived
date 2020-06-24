6a8cd56bf3566c1a5542b61c7ed6df6dd75584dc
Manolingam N <manolingam1996@gmail.com>
2020-06-24 19:34:19 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,223 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+
+var Airtable = require("airtable");
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+require("dotenv").config();
+
+Airtable.configure({
+    endpointUrl: "https://api.airtable.com",
+    apiKey: process.env.API_KEY,
+});
+
+// var raidcentral_base = new Airtable({ apiKey: process.env.API_KEY }).base(
+//     process.env.RAID_CENTRAL_BASE_ID
+// );
+
+var daoshop_base = Airtable.base(process.env.DAOSHOP_BASE_ID);
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs
+    .readdirSync("./commands/")
+    .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+const PREFIX = "!keeper";
+const MEMBER_ROLE_ID = process.env.MEMBER_ROLE_ID;
+const BOT_ID = process.env.BOT_ID;
+const HELP_MESSAGE =
+    "Hi! Am the Guild Keeper here. Here's my list of commands.\n\n" +
+    // "\t1. check-raids -- Lists the raids from Airtable.\n" +
+    // "\t2. check-status -- Gets the status of a raid from Airtable.\n" +
+    // "\t3. set-status -- Sets the status of a raid from Airtable.\n" +
+    "\t1. `create-raid` -- Creates a raid channel and initializes it.\n" +
+    "\t2. `create-rip` -- Creates a rip channel and initializes it.\n" +
+    "\t3. `crypt` -- Crypts an raid/rip/client channel under the battlefield and client chat category.\n" +
+    "\nI can also send in apprentice issues from any of the raidguild's repo to discord." +
+    "\tIf you want more info please see the **GuildKeeper docs:** https://hackmd.io/@saimano/guild-keeper\n" +
+    "\nFor help with a specific command type `!keeper help <command>`\n" +
+    "\nExample: for help with creating a raid channel, type `!keeper help create-raid`";
+
+let STATUS_CODES = {
+    0: "High Signal",
+    1: "Prepping for Battle",
+    2: "Yes",
+    3: "Active Raids",
+    4: "Completed",
+    5: "Failed",
+    6: "Declined",
+};
+
+let data = {};
+
+// const fetchRaids = async () => {
+//     await raidcentral_base("Raids")
+//         .select({
+//             fields: ["Name", "Status"],
+//             view: "Grid view",
+//         })
+//         .eachPage(
+//             function page(records, fetchNextPage) {
+//                 records.forEach((record) => {
+//                     data[record.fields.Name.toLowerCase()] = {
+//                         id: record.id,
+//                         status: record.fields.Status,
+//                     };
+//                 });
+//                 fetchNextPage();
+//             },
+//             function done(err) {
+//                 if (err) {
+//                     console.error(err);
+//                     return;
+//                 }
+//             }
+//         );
+// };
+
+app.post("/payload", (req, res) => {
+    if (req.body.action === "labeled") {
+        let issue = req.body.issue.html_url;
+        let title = req.body.issue.title;
+        let desc = req.body.issue.body || "No description provided.";
+        let state = req.body.issue.state;
+        let label = req.body.label.name;
+
+        let apprentice_issue =
+            "👨‍🎓 New apprentice issue created!\n\n" +
+            `**Title:** ${title}\n` +
+            `**Description:** ${desc}\n` +
+            `**State:** ${state}\n` +
+            `**Issue:** ${issue}`;
+
+        client.guilds.cache
+            .get(process.env.GUILD_ID)
+            .channels.cache.get("724252185877282936")
+            .send(apprentice_issue);
+    }
+
+    res.send("Received");
+});
+
+app.post("/daoshop", async (req, res) => {
+    let {
+        project_name,
+        summary,
+        skills_needed,
+        specs,
+        name,
+        email,
+        handle,
+        about_guild,
+        to_know,
+        slot_1,
+        slot_2,
+        slot_3,
+        transaction_hash,
+    } = req.body;
+    await daoshop_base("Bookings").create(
+        [
+            {
+                fields: {
+                    "Project Name": project_name,
+                    Summary: summary,
+                    "Skills Needed": skills_needed,
+                    "Do you have any specs?": specs,
+                    Name: name,
+                    Email: email,
+                    Handle: handle,
+                    "How did you hear about the guild?": about_guild,
+                    "Anything else you would like the guild to know?": to_know,
+                    "Booking Slot 1": slot_1,
+                    "Booking Slot 2": slot_2,
+                    "Booking Slot 3": slot_3,
+                    "Transaction Hash": transaction_hash,
+                },
+            },
+        ],
+        function (err, records) {
+            if (err) {
+                console.error(err);
+                return res.send(err);
+            }
+            records.forEach(function (record) {
+                let id = record.getId();
+                return res.send(id);
+            });
+        }
+    );
+});
+
+app.get("/", (req, res) => {
+    res.send("Hi");
+});
+
+client.on("ready", async () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    app.listen(process.env.PORT || 5000, () => console.log("Listening.."));
+    // fetchRaids();
+});
+
+client.on("message", (message) => {
+    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+    if (
+        !message.member.roles.member._roles.includes(process.env.MEMBER_ROLE_ID)
+    )
+        return message.channel.send("Only Members can command me!");
+
+    let args = message.content.slice(PREFIX.length).split(/ +/);
+    let command = args[1];
+
+    if (
+        args.length == 2 &&
+        message.content.startsWith(PREFIX) &&
+        args[1] === "help"
+    )
+        return message.channel.send(HELP_MESSAGE);
+
+    switch (command) {
+        case "help":
+            return client.commands.get("help").execute(message, args);
+        // case "check-raids":
+        //     return client.commands.get("check-raids").execute(message, raidcentral_raidcentral_base);
+        // case "check-status":
+        //     return client.commands
+        //         .get("check-status")
+        //         .execute(message, args, data);
+        // case "set-status":
+        //     return client.commands
+        //         .get("set-status")
+        //         .execute(message, args, data, MEMBER_ROLE_ID, STATUS_CODES);
+        case "create-raid":
+            return client.commands
+                .get("create-raid-rip")
+                .execute(message, args, BOT_ID, MEMBER_ROLE_ID);
+        case "create-rip":
+            return client.commands
+                .get("create-raid-rip")
+                .execute(message, args, BOT_ID, MEMBER_ROLE_ID);
+        case "crypt":
+            return client.commands
+                .get("crypt-raid-rip")
+                .execute(message, MEMBER_ROLE_ID);
+        default:
+            return message.channel.send(
+                "Invalid command! Check **!keeper help**."
+            );
+    }
+});
+
+client.login(process.env.TOKEN);
---
+++ commands/help.js
@@ -0,0 +1,34 @@
+module.exports = {
+    name: "help",
+    description: "Returns the list of available commands for use.",
+    execute(message, args) {
+        const CREATE_RAID = `To create a raid channel, use **!keeper create-raid [the-project-name (no spaces)] [link to the project proposal (must be a link)] [mention party member1] [mention party member2] [mention if anyone else is involved]**. By default, if no party members are mentioned, the person who sends this commands will be included without an explicit mention.`;
+        const CREATE_RIP = `To create a rip channel, use **!keeper create-rip [the-project-name (no spaces)] [link to the project proposal (must be a link)] [mention party member1] [mention party member2] [mention if anyone else is involved]**. By default, if no party members are mentioned, the person who sends this commands will be included without an explicit mention.`;
+        const CRYPT = `To crypt one, navigate to the raid/rip/client channel you wanna crypt and use **!keeper crypt**.`;
+        const CHECK_RAIDS = `To view all raids registered in Airtable, use **!keeper check-raids**.`;
+        const CHECK_STATUS = `To check the status of a particular raid from Airtable, use **!keeper check-status [raid-name]**.`;
+        const SET_STATUS = `To set/update the status of particular raid, use **!keeper set-status [raid-name] [STATUS_CODE]**. Below are the available status codes.\n\t**0** -- High Signal\n\t**1** -- Prepping for Battle\n\t**2** -- Yes\n\t**3** -- Active Raids\n\t**4** -- Completed\n\t**5** -- Failed\n\t**6** -- Declined.\n_NOTE: Only members can use this command. If a non-member uses it, other RG members will be alerted._`;
+
+        if (args.length < 3)
+            return message.channel.send("Missing command! Help with what?");
+
+        switch (args[2]) {
+            case "create-raid":
+                return message.channel.send(CREATE_RAID);
+            case "create-rip":
+                return message.channel.send(CREATE_RIP);
+            case "crypt":
+                return message.channel.send(CRYPT);
+            case "check-status":
+                return message.channel.send(CHECK_STATUS);
+            case "check-raids":
+                return message.channel.send(CHECK_RAIDS);
+            case "set-status":
+                return message.channel.send(SET_STATUS);
+            default:
+                return message.channel.send(
+                    "Hmm.. I don't know what that command does. Do you know?"
+                );
+        }
+    },
+};
